1d20972ea07dff8d2b7127763b0d95441dd35408
SadSAndWiCh _ <caiocamargo007@gmail.com>
2023-01-18 17:04:28 -0300

    feat(App): create home page using header and summary table components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-function App() {
+import { Header } from "./components/Header"
+import { SummaryTable } from "./components/SummaryTablet"
+
+export const App = () => {
   return (
-    <h1>Hello World</h1>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+
+        <SummaryTable />
+      </div>
+    </div>
   )
 }
-
-export default App
