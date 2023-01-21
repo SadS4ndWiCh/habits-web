65adc1de90c7c930c7850a9600087ecdec872437
SadSAndWiCh _ <caiocamargo007@gmail.com>
2023-01-21 14:33:55 -0300

    feat(accessibility): added focus styles to improve accessibility

--- src/components/NewHabitForm.tsx
+++ src/components/NewHabitForm.tsx
@@ -57,7 +57,7 @@ export const NewHabitForm = () => {
 				required
 				value={title}
 				onChange={event => setTitle(event.target.value)}
-				className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
+				className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400 focus:outline-none focus:ring-2 focus:ring-violet-600 focus:ring-offset-2 focus:ring-offset-zinc-900"
 			/>
 
 			<label
@@ -73,10 +73,10 @@ export const NewHabitForm = () => {
 						key={weekDay}
 						checked={weekDays.includes(i)}
 						onCheckedChange={() => handleToggleWeekDay(i)}
-						className='flex items-center gap-3 group'
+						className='flex items-center gap-3 group outline-none'
 					>
 						<div
-							className='w-8 h-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'
+							className='w-8 h-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500 transition-colors group-focus:ring-2 group-focus:ring-violet-600 group-focus:ring-offset-2 group-focus:ring-offset-background'
 						>
 							<Checkbox.Indicator>
 								<Check size={20} weight='bold' className='text-white'/>
@@ -92,7 +92,7 @@ export const NewHabitForm = () => {
 
 			<button
 				type="submit"
-				className="mt-6 rounded-lg p-4 gap-3 flex items-center justify-center font-semibold bg-green-600 hover:bg-green-500"
+				className="mt-6 rounded-lg p-4 gap-3 flex items-center justify-center font-semibold bg-green-600 hover:bg-green-500 transition-colors focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2 focus:ring-offset-zinc-900"
 			>
 				<Check size={20} weight="bold"/>
 				Confirmar
