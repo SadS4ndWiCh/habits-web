1058af9507e25a0a5cd3c6964bbedbfc9def08e1
SadSAndWiCh _ <caiocamargo007@gmail.com>
2023-01-21 17:55:01 -0300

    feat(react-query): wrap app with react query provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
 import './styles/global.css';
 import './libs/dayjs';
 
+import { QueryClient, QueryClientProvider } from 'react-query';
+
 import { Header } from "./components/Header"
 import { SummaryTable } from "./components/SummaryTablet"
 
+const queryClient = new QueryClient();
+
 export const App = () => {
   return (
-    <div className="w-screen h-screen flex justify-center items-center">
-      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
-        <Header />
+    <QueryClientProvider client={queryClient}>
+      <div className="w-screen h-screen flex justify-center items-center">
+        <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+          <Header />
 
-        <SummaryTable />
+          <SummaryTable />
+        </div>
       </div>
-    </div>
+    </QueryClientProvider>
   )
 }
